f74c52965cac52f35eb62166d33d5800e08e199b
bitch0mp <christiana.brewer@gmail.com>
2023-09-01 17:02:15 -0400

    testing deployment 09012023

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            This will smell amazing!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
